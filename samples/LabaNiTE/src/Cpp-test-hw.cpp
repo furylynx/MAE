586d5ca0f0be4e53d62694dd5c2a1c5ad0239e9f
fury lynx <furylynx@users.noreply.github.com>
2014-05-08 14:21:37 +0200

    LabaNiTE project structure created

---
+++ samples/LabaNiTE/src/Cpp-test-hw.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Cpp-test-hw.cpp
+// Author      : Børge
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello" << endl; // prints Hello
+	return 0;
+}
