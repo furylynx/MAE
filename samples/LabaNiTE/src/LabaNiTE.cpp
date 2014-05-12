af166d8f6081710b405696503daba4ca9ebfa6f8
fury lynx <furylynx@users.noreply.github.com>
2014-05-12 17:26:48 +0200

    project setup and includes

--- samples/LabaNiTE/src/LabaNiTE.cpp
+++ samples/LabaNiTE/src/LabaNiTE.cpp
@@ -7,9 +7,21 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
 
-int main() {
-	cout << "Hello Git" << endl; // prints Hello
+
+//TODO if you want to include boost use -lboost_* with * for the part used e.g. -lboost_system
+
+
+int main(int argc, char* argv[]) {
+
+	//just print startup message
+	std::cout << "LabaNiTE started!" << std::endl;
+
+
+
+
+	//just print exit message
+	std::cout << "LabaNiTE stopped!" << std::endl;
+
 	return 0;
 }
