9427c7228ddbf0ecde01b4ad0cdc20f9e127efde
fury lynx <furylynx@users.noreply.github.com>
2014-05-26 20:10:18 +0200

    Correct spherical representation using inclination ranging from 0 to 180 degree and azimuth -180 to 180 degree.

--- libmae/src/fl/FLSkeletonController.cpp
+++ libmae/src/fl/FLSkeletonController.cpp
@@ -33,10 +33,9 @@ namespace mae {
 
 			// fill all joints of torso into matrix in order to
 			// reduce it to three vectors that are used for the coordinate system
-			cv::Mat torso = cv::Mat(3,7,CV_64F);
-			//todo fill torso
-			std::vector<int> bodyParts = std::vector<int>();
+			cv::Mat torso = cv::Mat::zeros(3,6,CV_64F);
 
+			std::vector<int> bodyParts;
 			bodyParts.push_back(mae::model::GeneralSkeleton::SKEL_LEFT_SHOULDER);
 			bodyParts.push_back(mae::model::GeneralSkeleton::SKEL_RIGHT_SHOULDER);
 			bodyParts.push_back(mae::model::GeneralSkeleton::SKEL_NECK);
@@ -45,7 +44,11 @@ namespace mae {
 			bodyParts.push_back(mae::model::GeneralSkeleton::SKEL_RIGHT_HIP);
 
 			for(unsigned int i = 0; i < bodyParts.size(); i++) {
-				std::shared_ptr<mae::model::GeneralJoint> joint = skeleton->getJoint(i);
+				int bodyPart = bodyParts[i];
+				std::shared_ptr<mae::model::GeneralJoint> joint = skeleton->getJoint(bodyPart);
+
+				//todo if one of the vectors is invalid -> remove from matrix
+				//todo if less than 3 vectors -> invalid skeleton
 
 				torso.at<double>(0,i) = joint->getX();
 				torso.at<double>(1,i) = joint->getY();
@@ -55,18 +58,11 @@ namespace mae {
 
 			// apply PCA to get 3 principal components
 			cv::Mat torso_coord;
-			cv::PCA torso_pca = cv::PCA(torso, cv::Mat(), CV_PCA_DATA_AS_ROW, 2);
-			torso_pca.project(torso, torso_coord);
+			cv::PCA torso_pca = cv::PCA(torso, cv::Mat(), CV_PCA_DATA_AS_COL, 2);
 
 			// create basis {u,r,t}
-			cv::Vec3d u = torso_coord.col(0);
-			//todo align first vector (u) in top-down (pointing to the ground)
-			//todo other approach : align to head
-
-			cv::Vec3d r = torso_coord.col(1);
-			//left-right align
-			//todo other approach: align to left shoulder
-
+			cv::Vec3d u = torso_pca.eigenvectors.row(0).clone();
+			cv::Vec3d r = torso_pca.eigenvectors.row(1).clone();
 			cv::Vec3d t = u.cross(r);
 
 			//normalize
@@ -75,7 +71,24 @@ namespace mae {
 			t = cv::normalize(t);
 
 
+			//align vectors top-down/right-left
+			cv::Vec3d joint_torso = FLSkeletonController::jointToVec(skeleton->getJoint(mae::model::GeneralSkeleton::SKEL_TORSO));
+			cv::Vec3d joint_neck = FLSkeletonController::jointToVec(skeleton->getJoint(mae::model::GeneralSkeleton::SKEL_NECK));
 
+			cv::Vec3d joint_ls = FLSkeletonController::jointToVec(skeleton->getJoint(mae::model::GeneralSkeleton::SKEL_LEFT_SHOULDER));
+			cv::Vec3d joint_rs = FLSkeletonController::jointToVec(skeleton->getJoint(mae::model::GeneralSkeleton::SKEL_RIGHT_SHOULDER));
+
+			if (cv::norm(joint_neck - (joint_torso+u)) < cv::norm(joint_neck - joint_torso) )
+			{
+				u = -u;
+			}
+
+			if (cv::norm(joint_ls - (joint_rs+r)) > cv::norm(joint_ls - joint_rs) )
+			{
+				r = -r;
+			}
+
+			std::cout << std::endl;
 
 			// ---
 			// Calculate spherical coordinates
@@ -83,6 +96,7 @@ namespace mae {
 			// ---
 
 			std::shared_ptr<mae::fl::FLSkeleton> result = std::shared_ptr<mae::fl::FLSkeleton>(new mae::fl::FLSkeleton());
+
 
 
 			//fill vector with body parts in order to process them later
@@ -134,25 +148,28 @@ namespace mae {
 			extremities.push_back(joints);
 			joints.clear();
 
-			for (unsigned int i = 0; extremities.size(); i++){
+
+
+			for (unsigned int i = 0; i < extremities.size(); i++){
 				joints = extremities[i];
 				int joint_i = joints[0];
 				int joint_o = joints[1];
 				int joint_e = joints[2];
 
 				int joint_fl_i = joints[3];
-				int joint_fl_o = joints[5];
+				int joint_fl_o = joints[4];
 
-				cv::Vec2d angles = FLSkeletonController::calcFirstDegreeJoint(skeleton, joint_i, joint_o, r, t, u);
+				cv::Vec2d angles = FLSkeletonController::calcFirstDegreeJoint(skeleton, joint_i, joint_o, u, r, t);
 				std::shared_ptr<mae::fl::FLJoint> joint = std::shared_ptr<mae::fl::FLJoint> (new mae::fl::FLJoint());
 				joint->setTheta(angles[0]);
 				joint->setPhi(angles[1]);
 				result->setJoint(joint_fl_i, joint);
 
-				angles = FLSkeletonController::calcSecondDegreeJoint(skeleton, joint_i, joint_o, joint_e, r, t, u);
+				angles = FLSkeletonController::calcSecondDegreeJoint(skeleton, joint_i, joint_o, joint_e, u, r, t);
 				joint = std::shared_ptr<mae::fl::FLJoint> (new mae::fl::FLJoint());
 				joint->setTheta(angles[0]);
 				joint->setPhi(angles[1]);
+
 				result->setJoint(joint_fl_o, joint);
 			}
 
@@ -184,39 +201,33 @@ namespace mae {
 			// calculate angles for first degree joints
 			// ---
 
+			cv::Vec3d vec_i = FLSkeletonController::jointToVec(skeleton->getJoint(adjacent_joint));
+			cv::Vec3d vec_o = FLSkeletonController::jointToVec(skeleton->getJoint(outer_joint));
 
-			std::shared_ptr<mae::model::GeneralJoint> joint_i = skeleton->getJoint(adjacent_joint);
-			std::shared_ptr<mae::model::GeneralJoint> joint_o = skeleton->getJoint(outer_joint);
-
-			cv::Vec3d vec_i;
-			vec_i[0] = joint_i->getX();
-			vec_i[1] = joint_i->getY();
-			vec_i[2] = joint_i->getZ();
-
-			cv::Vec3d vec_o;
-			vec_o[0] = joint_o->getX();
-			vec_o[1] = joint_o->getY();
-			vec_o[2] = joint_o->getZ();
 
 			// get first degree bone vector
 			cv::Vec3d fdvec = cv::normalize(vec_o - vec_i);
 
 			// inclination theta
 			// calculates theta in radian
-			double theta = std::acos(u.dot(fdvec));
-			// ...to degree
-			theta *= 180 / M_PI;
+			double theta = FLSkeletonController::calcAngleHalfDeg(u, fdvec);
 
-
-			//get azimuth phi
-			cv::Vec3d vec_o_p = FLSkeletonController::projectOrthogonal(vec_o, vec_i, r, t);
-			cv::Vec3d fdvec_p = cv::normalize(vec_i - vec_o_p);
 
 			// calculates phi in radian
-			double phi = std::acos(r.dot(fdvec_p));
+			double phi;
 
-			// ...to degree
-			phi *= 180 / M_PI;
+			if (FLSkeletonController::areCollinear(u, fdvec))
+			{
+				phi = 0;
+			}
+			else
+			{
+				//get azimuth phi by projecting the joint on the r-t-plane
+				cv::Vec3d vec_o_p = FLSkeletonController::projectOrthogonal(vec_o, vec_i, r, t);
+				cv::Vec3d fdvec_p = cv::normalize(vec_i - vec_o_p);
+
+				phi = FLSkeletonController::calcAngleDeg(r, fdvec_p);
+			}
 
 			cv::Vec2d angles;
 			angles[0] = theta;
@@ -232,25 +243,10 @@ namespace mae {
 			// calculate angles for second degree joints
 			// ---
 
-			std::shared_ptr<mae::model::GeneralJoint> joint_i = skeleton->getJoint(adjacent_joint);
-			std::shared_ptr<mae::model::GeneralJoint> joint_o = skeleton->getJoint(outer_joint);
-			std::shared_ptr<mae::model::GeneralJoint> joint_lh = skeleton->getJoint(extremity_joint);
+			cv::Vec3d vec_i = FLSkeletonController::jointToVec(skeleton->getJoint(adjacent_joint));
+			cv::Vec3d vec_o = FLSkeletonController::jointToVec(skeleton->getJoint(outer_joint));
+			cv::Vec3d vec_e = FLSkeletonController::jointToVec(skeleton->getJoint(extremity_joint));
 
-
-			cv::Vec3d vec_i;
-			vec_i[0] = joint_i->getX();
-			vec_i[1] = joint_i->getY();
-			vec_i[2] = joint_i->getZ();
-
-			cv::Vec3d vec_o;
-			vec_o[0] = joint_o->getX();
-			vec_o[1] = joint_o->getY();
-			vec_o[2] = joint_o->getZ();
-
-			cv::Vec3d vec_e;
-			vec_e[0] = joint_lh->getX();
-			vec_e[1] = joint_lh->getY();
-			vec_e[2] = joint_lh->getZ();
 
 			// get first degree bone vector
 			cv::Vec3d fdvec = cv::normalize(vec_o - vec_i);
@@ -259,42 +255,42 @@ namespace mae {
 			cv::Vec3d sdvec = cv::normalize(vec_e- vec_o);
 
 			// calc inclination theta
-			// angle between fdvec and sdvec which is the angle between r_r and sdvec
-			double theta = std::acos(fdvec.dot(sdvec));
+			// angle between fdvec and sdvec which is the angle between r_r and sdvec (angle ranges from 0 to 180 degree
+			double theta = FLSkeletonController::calcAngleHalfDeg(fdvec, sdvec);
 
-			// ...to degree
-			theta *= 180 / M_PI;
 
 			// calc azimuth phi
 			double phi;
 
-			if (FLSkeletonController::areCollinear(fdvec, sdvec)){
+			if (FLSkeletonController::areCollinear(fdvec, sdvec))
+			{
 				// both joint vectors fdvec and sdvec are collinear
 				// therefore we can set phi to zero
 				phi = 0;
-			}else{
-
+			}
+			else
+			{
 
 				//rotate around axis b = v x r ...
 				cv::Vec3d b = cv::normalize(fdvec.cross(r));
 
 				// ... by rotation angle beta
-				double beta = std::acos(fdvec.dot(r));
+				//double beta = std::acos(fdvec.dot(r));
+				double beta = FLSkeletonController::calcAngle(fdvec, r);
 
+				//todo dont calculate the rotation matrix several times : efficiency!!
 				//apply to all
-				cv::Vec3d u_r = FLSkeletonController::rotateAroundAxis(u, b, beta);
+				cv::Vec3d u_r = FLSkeletonController::rotateAroundAxis(u, b, -beta);
 				// r is not needed to be rotated since the projection will work with the plane spanned by u and t
-				//cv::Vec3d r_r = FLSkeletonController::rotateAroundAxis(r, b, beta);
-				cv::Vec3d t_r = FLSkeletonController::rotateAroundAxis(t, b, beta);
+				//cv::Vec3d r_r = FLSkeletonController::rotateAroundAxis(r, b, -beta);
+				cv::Vec3d t_r = FLSkeletonController::rotateAroundAxis(t, b, -beta);
 
 				//plane othogonal to v is E(u,t)
-				cv::Vec3d sdvec_p = FLSkeletonController::projectOrthogonal(sdvec, vec_o, u_r, t_r);
+				cv::Vec3d sdvec_p = FLSkeletonController::projectOrthogonal(sdvec+vec_o, vec_o, u_r, t_r);
 				sdvec_p = cv::normalize(vec_o - sdvec_p);
-				phi = std::acos(t_r.dot(sdvec_p));
-			}
 
-			// ...to degree
-			phi *= 180 / M_PI;
+				phi = FLSkeletonController::calcAngleDeg(t_r, sdvec_p);
+			}
 
 
 			//return the angles
@@ -304,6 +300,15 @@ namespace mae {
 
 			return angles;
 
+		}
+
+		cv::Vec3d FLSkeletonController::jointToVec(std::shared_ptr<mae::model::GeneralJoint> joint){
+			cv::Vec3d result;
+
+			result[0] = joint->getX();
+			result[1] = joint->getY();
+			result[2] = joint->getZ();
+			return result;
 		}
 
 		cv::Vec3d FLSkeletonController::projectOrthogonal(cv::Vec3d point, cv::Vec3d position_vector, cv::Vec3d plane_u, cv::Vec3d plane_v){
@@ -366,6 +371,8 @@ namespace mae {
 			axis = cv::normalize(axis);
 			cv::Vec3d x;
 			x[0] = 1;
+			x[1] = 0;
+			x[2] = 0;
 
 			if (FLSkeletonController::areCollinear(axis, x)){
 				x[0] = 0;
@@ -396,10 +403,11 @@ namespace mae {
 			double sin_beta = std::sin(beta);
 
 			cv::Mat r_x = cv::Mat::zeros(3,3,CV_64F);
-			r_x.at<double>(0,0) = cos_beta;
-			r_x.at<double>(0,1) = -sin_beta;
-			r_x.at<double>(1,0) = sin_beta;
 			r_x.at<double>(1,1) = cos_beta;
+			r_x.at<double>(1,2) = -sin_beta;
+			r_x.at<double>(2,1) = sin_beta;
+			r_x.at<double>(2,2) = cos_beta;
+			r_x.at<double>(0,0) = 1;
 
 			cv::Mat result = m*r_x*m.inv()*p;
 			return result.rowRange(0,3);
@@ -409,27 +417,63 @@ namespace mae {
 
 			// collinear if a = beta * b;
 
-			//todo or other assumption: angle between both is 0 or 180 degree
+			// assumption: angle between both is 0 or 180 degree (seems to be more robust than calculating beta)
+			a = cv::normalize(a);
+			b = cv::normalize(b);
 
-			double beta;
+			//no need for angle ranging from 0-360 therefore this does the trick
+			double angle = FLSkeletonController::calcAngleHalf(a,b);//std::acos(a.dot(b));
 
-			for (int i = 0; i< 3; i++){
-				if (b[i] != 0){
-					beta = a[i]/b[i];
+			return angle == 0 || angle == M_PI || angle == -M_PI;
+		}
 
-					if (beta == 0){
-						return false;
-					}
 
-					return a[1] == beta*b[1] && a[2] == beta*b[2] && a[3] == beta*b[3];
-				}else{
-					if (a[i] != 0){
-						return false;
+		double FLSkeletonController::calcAngle(cv::Vec3d a, cv::Vec3d b){
+
+			double halfAngle = FLSkeletonController::calcAngleHalf(a,b);
+
+
+			if (halfAngle == 0 || halfAngle == M_PI || halfAngle == -M_PI){
+				return halfAngle;
+			}
+
+
+			//calc cross product
+			cv::Vec3d cross = a.cross(b);
+
+			int sign = (cross[2] > 0) - (cross[2] < 0);
+
+			if (sign == 0){
+				sign = (cross[1] > 0) - (cross[1] < 0);
+
+				if (sign == 0){
+					sign = (cross[0] > 0) - (cross[0] < 0);
+
+					if (sign == 0){
+						//todo throw exception ?
+						std::cerr << "This case should not occur" << std::endl;
+						return halfAngle;
 					}
 				}
 			}
 
-			return false;
+			return halfAngle*sign;
+		}
+
+		double FLSkeletonController::calcAngleDeg(cv::Vec3d a, cv::Vec3d b){
+			return FLSkeletonController::radToDeg(FLSkeletonController::calcAngle(a, b));
+		}
+
+		double FLSkeletonController::calcAngleHalf(cv::Vec3d a, cv::Vec3d b){
+			return std::acos(a.dot(b));
+		}
+
+		double FLSkeletonController::calcAngleHalfDeg(cv::Vec3d a, cv::Vec3d b){
+			return FLSkeletonController::radToDeg(FLSkeletonController::calcAngleHalf(a, b));
+		}
+
+		double FLSkeletonController::radToDeg(double val){
+			return val*180 / M_PI;
 		}
 
 	} // namespace fl
