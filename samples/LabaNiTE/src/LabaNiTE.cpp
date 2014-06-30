94a4f38fb643b844f21223b5001f5c850a5d04ed
fury lynx <furylynx@users.noreply.github.com>
2014-06-30 19:31:57 +0200

    Flags for bvh export or pose display.

--- samples/LabaNiTE/src/LabaNiTE.cpp
+++ samples/LabaNiTE/src/LabaNiTE.cpp
@@ -39,14 +39,22 @@ using namespace lni;
 int main()
 {
 	std::cout << "LabaNiTe started" << std::endl;
+
+	//not working currently
 	bool store_skels = false;
+
+	//trigger for bvh export
+	bool bvh_export = true;
+	unsigned int bvh_frames = 300; //30fps openni
 
 	mae::fl::fl_movement_controller move = mae::fl::fl_movement_controller();
 
-	//create and add listener
-	std::shared_ptr<nite_display> ndisp = std::shared_ptr<nite_display>(new nite_display());
-	move.add_listener(ndisp);
-	std::cout << "done" << std::endl;
+	if (!bvh_export)
+	{
+		//create and add listener
+		std::shared_ptr<nite_display> ndisp = std::shared_ptr<nite_display>(new nite_display());
+		move.add_listener(ndisp);
+	}
 
 	mae::fl::bvh_controller bvh_ctrl = mae::fl::bvh_controller();
 	std::vector<std::shared_ptr<mae::general_skeleton> > wb_skels;
@@ -66,56 +74,60 @@ int main()
 			// ofstream def
 			std::ofstream* a_file;
 
-			if (store_skels)
+//			if (store_skels)
+//			{
+//				// filename string
+//				std::ostringstream filename;
+//				filename << SKEL_STORE_PATH << "skeleton" << k << ".txt";
+//
+//				// create file
+//				a_file = new std::ofstream(filename.str());
+//
+//				// print header to file
+//				*a_file << "// OpenNi/NiTE Skeleton and MAE General Skeleton" << std::endl;
+//
+//				//print date to file
+//				std::time_t now = std::time(nullptr);
+//				std::tm now_local = *std::localtime(&now);
+//				*a_file << "// " << (now_local.tm_year + 1900) << '-' << (now_local.tm_mon + 1) << '-'
+//						<< now_local.tm_mday << " " << now_local.tm_hour << ":" << now_local.tm_min << ":"
+//						<< now_local.tm_sec << std::endl << std::endl;
+//
+//				//print skeleton initialization to file
+//				*a_file
+//						<< "std::shared_ptr<mae::model::GeneralSkeleton> skeleton = std::shared_ptr<mae::model::GeneralSkeleton>(new mae::model::GeneralSkeleton());"
+//						<< std::endl << std::endl;
+//
+//				//print joint assignment to file (for later use)
+////				*a_file << "// " << mae::maej_str[joint_ids_mae[j]] << " (" << xn_joint.position.position.X << ", "
+////						<< xn_joint.position.position.Y << ", " << xn_joint.position.position.Z << ")" << std::endl;
+////				*a_file << "skeleton->setJoint(" << (int) joint_ids_mae[j]
+////						<< ", std::shared_ptr<mae::model::GeneralJoint>(new mae::model::GeneralJoint("
+////						<< (double) xn_joint.position.position.X << ", " << (double) xn_joint.position.position.Y
+////						<< ", " << (double) xn_joint.position.position.Z << ")));" << std::endl;
+//
+//// Close the file stream explicitly
+//				a_file->close();
+//				delete a_file;
+//			}
+
+			if (bvh_export)
 			{
-				// filename string
-				std::ostringstream filename;
-				filename << SKEL_STORE_PATH << "skeleton" << k << ".txt";
+				if (wb_skels.size() > bvh_frames)
+				{
+					try
+					{
 
-				// create file
-				a_file = new std::ofstream(filename.str());
+						bvh_ctrl.bvh_file(wb_skels, "/home/keks/whole_sequence.bvh");
+						std::cout << ">>  BVH EXPORT DONE" << std::endl;
+						return 0;
 
-				// print header to file
-				*a_file << "// OpenNi/NiTE Skeleton and MAE General Skeleton" << std::endl;
-
-				//print date to file
-				std::time_t now = std::time(nullptr);
-				std::tm now_local = *std::localtime(&now);
-				*a_file << "// " << (now_local.tm_year + 1900) << '-' << (now_local.tm_mon + 1) << '-'
-						<< now_local.tm_mday << " " << now_local.tm_hour << ":" << now_local.tm_min << ":"
-						<< now_local.tm_sec << std::endl << std::endl;
-
-				//print skeleton initialization to file
-				*a_file
-						<< "std::shared_ptr<mae::model::GeneralSkeleton> skeleton = std::shared_ptr<mae::model::GeneralSkeleton>(new mae::model::GeneralSkeleton());"
-						<< std::endl << std::endl;
-
-				//print joint assignment to file (for later use)
-//				*a_file << "// " << mae::maej_str[joint_ids_mae[j]] << " (" << xn_joint.position.position.X << ", "
-//						<< xn_joint.position.position.Y << ", " << xn_joint.position.position.Z << ")" << std::endl;
-//				*a_file << "skeleton->setJoint(" << (int) joint_ids_mae[j]
-//						<< ", std::shared_ptr<mae::model::GeneralJoint>(new mae::model::GeneralJoint("
-//						<< (double) xn_joint.position.position.X << ", " << (double) xn_joint.position.position.Y
-//						<< ", " << (double) xn_joint.position.position.Z << ")));" << std::endl;
-
-// Close the file stream explicitly
-				a_file->close();
-				delete a_file;
+					} catch (...)
+					{
+						std::cout << "unknown exception occured" << std::endl;
+					}
+				}
 			}
-
-//			try
-//			{
-//				if (wb_skels.size() > 120)
-//				{
-//					bvh_ctrl.bvh_file(wb_skels, "/home/keks/whole_sequence.bvh");
-//					std::cout << "done" << std::endl;
-//					return 0;
-//				}
-//			}
-//			catch(...)
-//			{
-//				std::cout << "unknown exception occured" << std::endl;
-//			}
 
 			try
 			{
