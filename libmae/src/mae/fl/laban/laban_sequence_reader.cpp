d6db23aee54d26a581e9928f88e51241d5b1c171
fury lynx <furylynx@users.noreply.github.com>
2014-07-28 16:29:20 +0200

    Laban sequence reader -> continued.

--- libmae/src/mae/fl/laban/laban_sequence_reader.cpp
+++ libmae/src/mae/fl/laban/laban_sequence_reader.cpp
@@ -7,7 +7,6 @@
 
 #include "laban_sequence_reader.hpp"
 
-
 namespace mae
 {
 	namespace fl
@@ -15,7 +14,7 @@ namespace mae
 		namespace laban
 		{
 
-			std::shared_ptr<laban_sequence>  laban_sequence_reader::read_sequence(std::string xml_string)
+			std::shared_ptr<laban_sequence> laban_sequence_reader::read_sequence(std::string xml_string)
 			{
 				std::shared_ptr<laban_sequence> result;
 
@@ -54,14 +53,14 @@ namespace mae
 					}
 				}
 
-
 				//staff elements
 				xmlpp::NodeSet measures_node_set = root_node->find("/score/staff/measures");
 				unsigned int measures = 0;
 
 				if (measures_node_set.size() > 0)
 				{
-					measures = static_cast<unsigned int>(std::stoi(dynamic_cast<xmlpp::ContentNode*>(measures_node_set.at(0))->get_content().c_str()));
+					measures = static_cast<unsigned int>(std::stoi(
+							dynamic_cast<xmlpp::ContentNode*>(measures_node_set.at(0))->get_content().c_str()));
 				}
 
 				xmlpp::NodeSet time_unit_node_set = root_node->find("/score/staff/timing/timeUnit");
@@ -69,7 +68,8 @@ namespace mae
 
 				if (time_unit_node_set.size() > 0)
 				{
-					std::string time_unit_str = dynamic_cast<xmlpp::ContentNode*>(time_unit_node_set.at(0))->get_content();
+					std::string time_unit_str =
+							dynamic_cast<xmlpp::ContentNode*>(time_unit_node_set.at(0))->get_content();
 					time_unit = e_time_unit_c::parse(time_unit_str);
 				}
 
@@ -78,7 +78,8 @@ namespace mae
 
 				if (beat_duration_node_set.size() > 0)
 				{
-					beat_duration = static_cast<unsigned int>(std::stoi(dynamic_cast<xmlpp::ContentNode*>(beat_duration_node_set.at(0))->get_content()));
+					beat_duration = static_cast<unsigned int>(std::stoi(
+							dynamic_cast<xmlpp::ContentNode*>(beat_duration_node_set.at(0))->get_content()));
 				}
 
 				xmlpp::NodeSet beats_node_set = root_node->find("/score/staff/timing/measure/beats");
@@ -86,11 +87,13 @@ namespace mae
 
 				if (beats_node_set.size() > 0)
 				{
-					beats = static_cast<unsigned int>(std::stoi(dynamic_cast<xmlpp::ContentNode*>(beats_node_set.at(0))->get_content()));
+					beats = static_cast<unsigned int>(std::stoi(
+							dynamic_cast<xmlpp::ContentNode*>(beats_node_set.at(0))->get_content()));
 				}
 
 				//create the sequence and add additional information as well as columns and movements
-				result = std::shared_ptr<laban_sequence>(new laban_sequence(title, authors.at(0), measures, time_unit, beat_duration, beats));
+				result = std::shared_ptr<laban_sequence>(
+						new laban_sequence(title, authors.at(0), measures, time_unit, beat_duration, beats));
 
 				//additional elements
 
@@ -99,9 +102,9 @@ namespace mae
 
 				if (description_node_set.size() > 0)
 				{
-					result->set_description(dynamic_cast<xmlpp::ContentNode*>(description_node_set.at(0))->get_content());
+					result->set_description(
+							dynamic_cast<xmlpp::ContentNode*>(description_node_set.at(0))->get_content());
 				}
-
 
 				//column definitions
 				xmlpp::NodeSet coldefs_node_set = root_node->find("/score/columns/columnDefinition");
@@ -110,7 +113,6 @@ namespace mae
 				{
 					result->add_column_definition(read_column_definition(coldefs_node_set.at(i)));
 				}
-
 
 				//movements
 				xmlpp::NodeSet movs_node_set = root_node->find("/score/movements/movement");
@@ -135,7 +137,8 @@ namespace mae
 
 				if (index_node_set.size() > 0)
 				{
-				 	index = static_cast<unsigned int>(std::stoi(dynamic_cast<xmlpp::ContentNode*>(index_node_set.at(0))->get_content()));
+					index = static_cast<unsigned int>(std::stoi(
+							dynamic_cast<xmlpp::ContentNode*>(index_node_set.at(0))->get_content()));
 				}
 
 				xmlpp::NodeSet body_part_node_set = node->find("/preSign/bodyPart");
@@ -147,29 +150,38 @@ namespace mae
 
 					if (side_node_set.size() > 0)
 					{
-						side = ps::e_side_c::parse(dynamic_cast<xmlpp::ContentNode*>(side_node_set.at(0))->get_content());
+						side = ps::e_side_c::parse(
+								dynamic_cast<xmlpp::ContentNode*>(side_node_set.at(0))->get_content());
 					}
 
+					std::shared_ptr<ps::i_endpoint> endpoint = laban_sequence_reader::read_end_point(body_part_node_set.at(0));
 
-					//area pre-sign
-					xmlpp::NodeSet area_node_set = node->find("/preSign/bodyPart/part");
-
-					if (side_node_set.size() > 0)
+					if (endpoint != nullptr)
 					{
-						ps::e_area area = ps::e_area_c::parse(dynamic_cast<xmlpp::ContentNode*>(area_node_set.at(0))->get_content());
-
-						pre_sign = std::shared_ptr<ps::i_pre_sign>(new ps::body_part(side, std::shared_ptr<ps::i_part>(new ps::area_part(area))));
+						pre_sign = std::shared_ptr<ps::i_pre_sign>(new ps::body_part(side, endpoint));
 					}
 					else
 					{
-						if (true)//TODO next part
-						{
+						std::shared_ptr<ps::i_limb> limb = laban_sequence_reader::read_limb(body_part_node_set.at(0));
 
+						if (limb != nullptr)
+						{
+							pre_sign = std::shared_ptr<ps::i_pre_sign>(new ps::body_part(side, limb));
+						}
+						else
+						{
+							std::shared_ptr<ps::surface_part> surface = laban_sequence_reader::read_surface(body_part_node_set.at(0));
+
+							if (surface != nullptr)
+							{
+								pre_sign = std::shared_ptr<ps::i_pre_sign>(new ps::body_part(side, surface));
+							}
+							else
+							{
+								throw std::invalid_argument("Column definition is invalid. Expected a body part, namely an endpoint a limb or a surface.");
+							}
 						}
 					}
-
-
-					//TODO any other information
 				}
 				else
 				{
@@ -200,6 +212,96 @@ namespace mae
 				}
 
 				result = std::shared_ptr<column_definition>(new column_definition(index, pre_sign));
+
+				return result;
+			}
+
+			std::shared_ptr<ps::i_endpoint> laban_sequence_reader::read_end_point(xmlpp::Node* node)
+			{
+				std::shared_ptr<ps::i_endpoint> result;
+
+				//area pre-sign
+				xmlpp::NodeSet area_node_set = node->find("/part");
+
+				if (area_node_set.size() > 0)
+				{
+					ps::e_area area = ps::e_area_c::parse(
+							dynamic_cast<xmlpp::ContentNode*>(area_node_set.at(0))->get_content());
+
+					result = std::shared_ptr<ps::i_endpoint>(new ps::area_part(area));
+				}
+				else
+				{
+					//joint pre-sign
+					xmlpp::NodeSet joint_node_set = node->find("/joint/joint");
+
+					if (joint_node_set.size() > 0)
+					{
+						ps::e_joint joint = ps::e_joint_c::parse(
+								dynamic_cast<xmlpp::ContentNode*>(joint_node_set.at(0))->get_content());
+
+						result = std::shared_ptr<ps::i_endpoint>(new ps::joint_part(joint));
+					}
+					else
+					{
+						//digit pre-sign
+						xmlpp::NodeSet digit_node_set = node->find("/digit/digit");
+
+						if (digit_node_set.size() > 0)
+						{
+							ps::e_digit digit = ps::e_digit_c::parse(
+									dynamic_cast<xmlpp::ContentNode*>(digit_node_set.at(0))->get_content());
+
+							xmlpp::NodeSet digit_joint_node_set = node->find("/digit/joint");
+							unsigned int knuckle = 0;
+							if (digit_joint_node_set.size() > 0)
+							{
+								knuckle = static_cast<unsigned int>(std::stoi(
+										dynamic_cast<xmlpp::ContentNode*>(digit_joint_node_set.at(0))->get_content()));
+							}
+
+							result = std::shared_ptr<ps::i_endpoint> (new ps::digit_part(digit, knuckle));
+						}
+					}
+				}
+
+				return result;
+			}
+
+			std::shared_ptr<ps::i_limb> laban_sequence_reader::read_limb(xmlpp::Node* node)
+			{
+				std::shared_ptr<ps::i_limb> result;
+
+				//default limb pre-sign
+				xmlpp::NodeSet default_limb_node_set = node->find(
+						"/limb/default/limb");
+
+				if (default_limb_node_set.size() > 0)
+				{
+					ps::e_limb limb =
+							ps::e_limb_c::parse(
+									dynamic_cast<xmlpp::ContentNode*>(default_limb_node_set.at(0))->get_content());
+
+					result = std::shared_ptr<ps::i_limb>(new ps::default_limb(limb));
+				}
+				else
+				{
+					//TODO
+					//custom limb?!
+					//surface?!
+
+					throw std::invalid_argument(
+							"custom limb and surface pre-sign are not yet implemented.");
+				}
+
+				return result;
+			}
+
+			std::shared_ptr<ps::surface_part> laban_sequence_reader::read_surface(xmlpp::Node* node)
+			{
+				std::shared_ptr<ps::surface_part> result;
+
+				//TODO read xml
 
 				return result;
 			}
--- libmae/src/mae/fl/laban/ps/e_joint.hpp
+++ libmae/src/mae/fl/laban/ps/e_joint.hpp
@@ -12,11 +12,12 @@
 #include "../../../indexer_fix.hpp"
 
 //custom includes
-//...
+#include "../../../mstr.hpp"
 
 //global includes
 #include <string>
 #include <stdexcept>
+#include <vector>
 
 namespace mae
 {
@@ -46,6 +47,8 @@ namespace mae
 				{
 					public:
 						static std::string str(e_joint joint_);
+						static std::vector<e_joint> vec();
+						static e_joint parse(std::string str);
 				};
 
 
