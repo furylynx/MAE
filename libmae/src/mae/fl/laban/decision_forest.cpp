fbb5f7a2a9802c8bdb2b60a53f2a6c45952eaa5e
fury lynx <furylynx@users.noreply.github.com>
2014-08-13 12:10:22 +0200

    Decision forest for sequence recognition -> continued.

--- libmae/src/mae/fl/laban/decision_forest.cpp
+++ libmae/src/mae/fl/laban/decision_forest.cpp
@@ -15,7 +15,7 @@ namespace mae
 		{
 
 			decision_forest::decision_forest(std::vector<int> body_parts,
-					std::shared_ptr<i_decision_maker> decision_maker)
+					std::shared_ptr<i_decision_maker<i_movement> > decision_maker)
 			{
 				body_parts_ = body_parts;
 				decision_maker_ = decision_maker;
@@ -27,13 +27,15 @@ namespace mae
 
 			void decision_forest::add_sequence(std::shared_ptr<laban_sequence> sequence)
 			{
+				//TODO insert not only the sequence but all rewritten sequences
+
 				for (unsigned int i = 0; i < body_parts_.size(); i++)
 				{
 					int body_part = body_parts_.at(i);
 
 					std::shared_ptr<i_movement> decision_item = sequence->get_column_movements(body_part).back();
 
-					std::vector<std::shared_ptr<decision_tree> > tree_list;
+					std::vector<std::shared_ptr<decision_tree<i_movement, laban_sequence> > > tree_list;
 					bool listed = false;
 
 					if (trees_.find(body_part) != trees_.end())
@@ -45,7 +47,10 @@ namespace mae
 							if (decision_maker_->decide(decision_item,
 									tree_list.at(j)->get_root()->get_decision_item()))
 							{
-								tree_list.at(j)->add_sequence(sequence);
+								tree_list.at(j)->add_sequence(
+										std::shared_ptr<decision_value<i_movement, laban_sequence> >(
+												new decision_value<i_movement, laban_sequence>(
+														sequence->get_column_movements(body_part), sequence)));
 								listed = true;
 								break;
 							}
@@ -55,8 +60,12 @@ namespace mae
 					if (!listed)
 					{
 						tree_list.push_back(
-								std::shared_ptr<decision_tree>(new decision_tree(body_part, decision_maker_)));
-						tree_list.back()->add_sequence(sequence);
+								std::shared_ptr<decision_tree<i_movement, laban_sequence> >(
+										new decision_tree<i_movement, laban_sequence>(decision_maker_)));
+						tree_list.back()->add_sequence(
+								std::shared_ptr<decision_value<i_movement, laban_sequence> >(
+										new decision_value<i_movement, laban_sequence>(
+												sequence->get_column_movements(body_part), sequence)));
 
 						if (trees_.find(body_part) != trees_.end())
 						{
@@ -82,21 +91,27 @@ namespace mae
 
 					std::shared_ptr<i_movement> decision_item = whole_sequence->get_column_movements(body_part).back();
 
-					std::vector<std::shared_ptr<decision_tree> > tree_list = trees_.at(body_part);
+					std::vector<std::shared_ptr<decision_tree<i_movement, laban_sequence> > > tree_list = trees_.at(
+							body_part);
 
-					std::vector<std::shared_ptr<laban_sequence> > tmp_seqs;
+					std::vector<std::shared_ptr<decision_value<i_movement, laban_sequence> > > tmp_seqs;
 
 					for (unsigned int j = 0; j < tree_list.size(); j++)
 					{
 						if (decision_maker_->decide(decision_item, tree_list.at(j)->get_root()->get_decision_item()))
 						{
-							tmp_seqs = tree_list.at(j)->find_submatches(whole_sequence);
+							tmp_seqs = tree_list.at(j)->find_submatches(
+									whole_sequence->get_column_movements(body_part));
 						}
 					}
 
 					if (i == 0)
 					{
-						result = tmp_seqs;
+						for (unsigned int j = 0; j < tmp_seqs.size(); j++)
+						{
+							result.push_back(tmp_seqs.at(j)->get_value());
+						}
+
 					}
 					else
 					{
@@ -107,7 +122,7 @@ namespace mae
 						{
 							for (unsigned int k = 0; k < result.size(); k++)
 							{
-								if (result.at(k) == tmp_seqs.at(j))
+								if (result.at(k) == tmp_seqs.at(j)->get_value())
 								{
 									tmp_result.push_back(result.at(k));
 
