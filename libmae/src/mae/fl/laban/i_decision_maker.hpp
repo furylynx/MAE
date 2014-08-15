df742b210cf5059d3e8c2b69cbdd78b0fe8d6400
fury lynx <furylynx@users.noreply.github.com>
2014-08-15 13:02:14 +0200

    Decision forest for sequence recognition -> continued.

--- libmae/src/mae/fl/laban/i_decision_maker.hpp
+++ libmae/src/mae/fl/laban/i_decision_maker.hpp
@@ -32,7 +32,24 @@ namespace mae
 				public:
 					virtual ~i_decision_maker(){}
 
+					/**
+					 * Checks whether the two elements match in order to provide the decision result.
+					 *
+					 * @param a The first element.
+					 * @param b The second element.
+					 * @return True if elements match.
+					 */
 					virtual bool decide(std::shared_ptr<T> a, std::shared_ptr<T> b);
+
+					/**
+					 * Checks the distance between the first to parameters and then examines whether the third parameter is okay.
+					 *
+					 * @param dist_from The distance from.
+					 * @param dist_to The distance to.
+					 * @param check The element to be checked to have enough distance.
+					 * @return True if distance is okay.
+					 */
+					virtual bool distance_okay(std::shared_ptr<T> dist_from, std::shared_ptr<T> dist_to, std::shared_ptr<T> check);
 			};
 
 		} // namespace laban
