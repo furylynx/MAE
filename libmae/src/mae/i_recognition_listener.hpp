2f93e1d82295bb6c0da8b1650fe0b8c58ac3fcc1
fury lynx <furylynx@users.noreply.github.com>
2014-08-30 13:36:04 +0200

    recognition listener for recognized sequences

---
+++ libmae/src/mae/i_recognition_listener.hpp
@@ -0,0 +1,45 @@
+/*
+ * i_recognition_listener.hpp
+ *
+ *  Created on: 30.08.2014
+ *      Author: keks
+ */
+
+#ifndef I_RECOGNITION_LISTENER_HPP_
+#define I_RECOGNITION_LISTENER_HPP_
+
+//eclipse indexer fix
+#include "indexer_fix.hpp"
+
+//custom includes
+//...
+
+//global includes
+#include <vector>
+#include <memory>
+
+
+namespace mae
+{
+
+	/**
+	 * Listener for poses. In notified each frame after the pose quantization.
+	 */
+	template <typename U>
+	class i_recognition_listener
+	{
+		public:
+			virtual ~i_recognition_listener(){}
+
+			/**
+			 * Is invoked each time sequences were recognized.
+			 *
+			 * @param timestamp The associated timestamp.
+			 * @param sequences The recognized sequences.
+			 */
+			virtual void on_recognition(long timestamp, std::vector<std::shared_ptr<U> > sequences) = 0;
+	};
+
+} // namespace mae
+
+#endif // I_RECOGNITION_LISTENER_HPP_
--- libmae/src/mae/i_sequence_listener.hpp
+++ libmae/src/mae/i_sequence_listener.hpp
@@ -19,12 +19,21 @@
 
 namespace mae
 {
+	/**
+	 * Listener for sequences. Is invoked each time a sequence was generated (which occurs in each frame).
+	 */
 	template <typename U>
 	class i_sequence_listener
 	{
 		public:
 			virtual ~i_sequence_listener(){}
 
+			/**
+			 * Is invoked each time a sequence was generated (which occurs on every frame).
+			 *
+			 * @param timestamp The associated timestamp.
+			 * @param sequence The generated sequence.
+			 */
 			virtual void on_sequence(long timestamp, std::shared_ptr<U> sequence) = 0;
 	};
 
