a91a7f46e034971bdca703e649624ae29740edbb
fury lynx <furylynx@users.noreply.github.com>
2014-09-17 00:06:51 +0200

    header files for packages.

---
+++ libmae_nite/src/mae/nite/nite.hpp
@@ -0,0 +1,15 @@
+/*
+ * nite.hpp
+ *
+ *  Created on: 16.09.2014
+ *      Author: keks
+ */
+
+#ifndef NITE_HPP_
+#define NITE_HPP_
+
+//custom includes
+#include "nite_controller.hpp"
+
+
+#endif // NITE_HPP_
