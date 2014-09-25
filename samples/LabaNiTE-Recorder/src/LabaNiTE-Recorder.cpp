bdf1977523207d54791a694b8cb0a00ebc8592d1
fury lynx <furylynx@users.noreply.github.com>
2014-09-25 20:00:26 +0200

    LabaNiTE-Server config continued. LabaNiTE-Recorder created.

---
+++ samples/LabaNiTE-Recorder/src/LabaNiTE-Recorder.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : LabaNiTE-Recorder.cpp
+// Author      : Børge
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "LabaNiTE-Recorder started." << std::endl;
+
+	//TODO
+
+
+	return 0;
+}
