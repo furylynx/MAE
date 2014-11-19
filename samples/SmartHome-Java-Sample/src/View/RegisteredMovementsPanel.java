08e6217b5846e9fcb7a8384af907766a974ef5c3
fury lynx <furylynx@users.noreply.github.com>
2014-11-19 13:03:16 +0100

    Java App for colloq -> continued.

--- samples/SmartHome-Java-Sample/src/View/RegisteredMovementsPanel.java
+++ samples/SmartHome-Java-Sample/src/View/RegisteredMovementsPanel.java
@@ -1,20 +1,33 @@
 package View;
 
+import java.awt.BorderLayout;
 import java.util.List;
 
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+
+import org.apache.batik.swing.JSVGCanvas;
+import org.apache.batik.swing.JSVGScrollPane;
 
 import maejava.LabanSequence;
 
 public class RegisteredMovementsPanel extends JPanel {
 
 	List<LabanSequence> registeredSequences;
+	JComboBox<String> comboBox;
+	List<JSVGScrollPane> svgPanels;
 	
 	public RegisteredMovementsPanel(List<LabanSequence> registeredSequences)
 	{
 		this.registeredSequences = registeredSequences;
 		
+		comboBox = new JComboBox<String>();
+		
 		//TODO selector + display for the sequences
+		
+		setLayout(new BorderLayout());
+		add(comboBox, BorderLayout.WEST);
+		
 	}
 	
 }
