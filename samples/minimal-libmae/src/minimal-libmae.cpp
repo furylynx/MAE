fd15bba33da46673eb4679871bed5143ee898f62
fury lynx <furylynx@users.noreply.github.com>
2015-02-02 17:59:39 +0100

    minimal-sample created for testing libmae builds.

---
+++ samples/minimal-libmae/src/minimal-libmae.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : minimal-libmae.cpp
+// Author      : fury lynx
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+#include <mae/mae.hpp>
+
+int main()
+{
+	std::cout << ">> Hello libmae <<" << std::endl;
+
+	mae::math::vec3d vec = mae::math::vec3d(1,2,3);
+
+	std::cout << vec << std::endl;
+
+
+	return 0;
+}
