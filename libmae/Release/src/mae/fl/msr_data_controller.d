4fc8e1bb9733e0eed13262be8e1fcb819fea6c94
fury lynx <furylynx@users.noreply.github.com>
2015-04-05 23:27:19 +0200

    msr data controller - continued.

---
+++ libmae/Release/src/mae/fl/msr_data_controller.d
@@ -0,0 +1,51 @@
+src/mae/fl/msr_data_controller.d: ../src/mae/fl/msr_data_controller.cpp \
+ ../src/mae/fl/msr_data_controller.hpp ../src/mae/fl/../indexer_fix.hpp \
+ ../src/mae/fl/../general_skeleton.hpp ../src/mae/fl/../indexer_fix.hpp \
+ ../src/mae/fl/../general_joint.hpp ../src/mae/fl/../math/vec3d.hpp \
+ ../src/mae/fl/../math/../indexer_fix.hpp ../src/mae/fl/../hierarchy.hpp \
+ ../src/mae/fl/../e_joint.hpp ../src/mae/fl/../mstr.hpp \
+ ../src/mae/fl/../hierarchy_element.hpp ../src/mae/fl/../bone.hpp \
+ ../src/mae/fl/../e_bone.hpp ../src/mae/fl/../math/basis.hpp \
+ ../src/mae/fl/../math/vec3d.hpp ../src/mae/fl/../mstr.hpp \
+ ../src/mae/fl/msr_data.hpp ../src/mae/fl/msr_spec.hpp \
+ ../src/mae/fl/../hierarchy.hpp ../src/mae/fl/../hierarchy_element.hpp
+
+../src/mae/fl/msr_data_controller.hpp:
+
+../src/mae/fl/../indexer_fix.hpp:
+
+../src/mae/fl/../general_skeleton.hpp:
+
+../src/mae/fl/../indexer_fix.hpp:
+
+../src/mae/fl/../general_joint.hpp:
+
+../src/mae/fl/../math/vec3d.hpp:
+
+../src/mae/fl/../math/../indexer_fix.hpp:
+
+../src/mae/fl/../hierarchy.hpp:
+
+../src/mae/fl/../e_joint.hpp:
+
+../src/mae/fl/../mstr.hpp:
+
+../src/mae/fl/../hierarchy_element.hpp:
+
+../src/mae/fl/../bone.hpp:
+
+../src/mae/fl/../e_bone.hpp:
+
+../src/mae/fl/../math/basis.hpp:
+
+../src/mae/fl/../math/vec3d.hpp:
+
+../src/mae/fl/../mstr.hpp:
+
+../src/mae/fl/msr_data.hpp:
+
+../src/mae/fl/msr_spec.hpp:
+
+../src/mae/fl/../hierarchy.hpp:
+
+../src/mae/fl/../hierarchy_element.hpp:
---
+++ libmae/Release/src/mae/fl/msr_spec.d
@@ -0,0 +1,19 @@
+src/mae/fl/msr_spec.d: ../src/mae/fl/msr_spec.cpp \
+ ../src/mae/fl/msr_spec.hpp ../src/mae/fl/../indexer_fix.hpp \
+ ../src/mae/fl/../hierarchy.hpp ../src/mae/fl/../indexer_fix.hpp \
+ ../src/mae/fl/../e_joint.hpp ../src/mae/fl/../mstr.hpp \
+ ../src/mae/fl/../hierarchy_element.hpp
+
+../src/mae/fl/msr_spec.hpp:
+
+../src/mae/fl/../indexer_fix.hpp:
+
+../src/mae/fl/../hierarchy.hpp:
+
+../src/mae/fl/../indexer_fix.hpp:
+
+../src/mae/fl/../e_joint.hpp:
+
+../src/mae/fl/../mstr.hpp:
+
+../src/mae/fl/../hierarchy_element.hpp:
