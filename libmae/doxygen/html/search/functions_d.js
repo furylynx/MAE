c894c4cfca9f9579d559d13cb433d6274de7f2c9
fury lynx <furylynx@users.noreply.github.com>
2015-05-23 19:51:29 +0200

    doxygen updated

--- libmae/doxygen/html/search/functions_d.js
+++ libmae/doxygen/html/search/functions_d.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['next_5fframe',['next_frame',['../classmae_1_1fl_1_1fl__movement__controller.html#a3eafa33ebaf85a140bcafdd1670b368c',1,'mae::fl::fl_movement_controller::next_frame()'],['../classmae_1_1movement__controller.html#aa153304725199cb7d4664a0f450ec934',1,'mae::movement_controller::next_frame()']]],
-  ['notify_5flisteners',['notify_listeners',['../classmae_1_1i__movement__detector.html#a347855d153525d7e09f793ff2b17b58b',1,'mae::i_movement_detector::notify_listeners()'],['../classmae_1_1kp__movement__detector.html#a55fb6a221a2d067db480055a2dfb7a66',1,'mae::kp_movement_detector::notify_listeners()']]],
-  ['notify_5frecognition_5flisteners',['notify_recognition_listeners',['../classmae_1_1movement__controller.html#a3b69de4b97cf639e621692ec8c8a9258',1,'mae::movement_controller']]],
-  ['notify_5fsequence_5flisteners',['notify_sequence_listeners',['../classmae_1_1movement__controller.html#a8e11558479a37a804102c94cfddde14d',1,'mae::movement_controller']]]
+  ['next_5fframe',['next_frame',['../classmae_1_1fl_1_1fl__movement__controller.html#a5755257514da363508ec487cb0ae13fb',1,'mae::fl::fl_movement_controller::next_frame()'],['../classmae_1_1movement__controller.html#a54f9cf019174baf78a5fba4fa2703952',1,'mae::movement_controller::next_frame()']]],
+  ['normalize',['normalize',['../classmae_1_1math_1_1vec3d.html#a3b96adb108c895bd488759ceecd40205',1,'mae::math::vec3d']]],
+  ['notify_5flisteners',['notify_listeners',['../classmae_1_1i__movement__detector.html#aebdc95347866bc2bc6b793383585fe2f',1,'mae::i_movement_detector::notify_listeners()'],['../classmae_1_1kp__movement__detector.html#acdc61f7edc7fdaadb5134e6cabd04612',1,'mae::kp_movement_detector::notify_listeners()']]],
+  ['notify_5frecognition_5flisteners',['notify_recognition_listeners',['../classmae_1_1movement__controller.html#af4bba61aefd94014f007192bf4b0da46',1,'mae::movement_controller']]],
+  ['notify_5fsequence_5flisteners',['notify_sequence_listeners',['../classmae_1_1movement__controller.html#a2bd72c0cb02311a7cddb4bd8898d8028',1,'mae::movement_controller']]]
 ];
