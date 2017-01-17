3b7913dfc5f26e09794436b440420fca5b03c68c
Börge Kordts <kordts@itm.uni-luebeck.de>
2017-01-17 08:52:11 +0100

    fixes for autotools, gitignore

--- libmae_eventing/autogen.sh
+++ libmae_eventing/autogen.sh
@@ -1,6 +1,14 @@
- #! /bin/sh
+#!/bin/bash
 
-libtoolize \
+LIBTOOLIZE=libtoolize
+
+if [ "$(uname)" == "Darwin" ]; then
+	# Mac OS X platform
+	# prepended g by brew or macports since libtool already in use
+	LIBTOOLIZE=glibtoolize	
+fi
+
+$LIBTOOLIZE \
 && aclocal \
 && automake --add-missing \
 && autoconf
