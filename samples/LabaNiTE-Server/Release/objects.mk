d5c348dd6b0c695856910eb863f64b7fab8d65a7
Börge Kordts <kordts@itm.uni-luebeck.de>
2017-02-13 12:23:44 +0100

    rm

--- samples/LabaNiTE-Server/Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lmae -lboost_system -lboost_filesystem -lmae_eventing -lmae_nite -lmae_demo
-
--- samples/LabaNiTE-Server/sequences/readme.txt
+++
@@ -1,1 +0,0 @@
-Sequences that are meant to be registered should be placed in this folder. Only files with *.laban file extension are processed.
