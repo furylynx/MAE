5df94c0f8df99d4878b1efa42ab6df52feb07e79
fury lynx <boergekordts@gmx.de>
2018-07-15 12:53:58 +0200

    check steps

--- .travis/docker_push.sh
+++ .travis/docker_push.sh
@@ -1,8 +1,21 @@
 #/bin/bash
 
+check_err() {
+   if [ $1 -ne 0 ]; then
+      echo "Failure: $2"
+      exit $1
+   fi
+}
+
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+check_err $? "docker login"
+
 VERSION_MAJOR=$(cat mae.json | jq -r .version_major)
 VERSION_MINOR=$(cat mae.json | jq -r .version_minor)
 VERSION_PATCH=$(cat mae.json | jq -r .version_patch)
-docker build --tag furylynx/mae:$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH --tag .
+
+docker build --tag furylynx/mae:$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH --tag furylynx/mae:$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH:latest .
+check_err $? "docker build"
+
 docker push furylynx/mae:$VERSION_MAJOR.$VERSION_MINOR.$VERSION_PATCH
+check_err $? "docker push"
