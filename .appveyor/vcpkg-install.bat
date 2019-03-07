33b4a12c5defbeddb298be7e9e4f40ecd315c54f
fury lynx <furylynx@users.noreply.github.com>
2019-03-07 15:56:16 +0100

    ci

--- .appveyor/vcpkg-install.bat
+++ .appveyor/vcpkg-install.bat
@@ -1,11 +1,13 @@
 cinst swig gradle maven jdk8 groovy
 cinst cmake --installargs 'ADD_CMAKE_TO_PATH=System'
-refreshenv
 
 cd C:\tools\vcpkg
+dir
 git pull
 .\bootstrap-vcpkg.bat
 vcpkg integrate install
 vcpkg install opencv libxmlpp sdl2 sdl2-image sdl2-ttf boost-core boost-asio boost-system
+
+refreshenv
 
 cd %APPVEYOR_BUILD_FOLDER%
