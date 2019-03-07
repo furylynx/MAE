792511237752b46651bd38824601accff0ea8dbd
fury lynx <furylynx@users.noreply.github.com>
2019-03-07 16:54:04 +0100

    ci

--- .appveyor/vcpkg-install.bat
+++ .appveyor/vcpkg-install.bat
@@ -1,13 +1,16 @@
 cinst swig gradle maven jdk8 groovy
 cinst cmake --installargs 'ADD_CMAKE_TO_PATH=System'
+refreshenv
 
 cd C:\tools\vcpkg
-dir
 git pull
 .\bootstrap-vcpkg.bat
-vcpkg integrate install
-vcpkg install opencv libxmlpp sdl2 sdl2-image sdl2-ttf boost-core boost-asio boost-system
+echo "vcpkg bootstrapped"
 
-refreshenv
+.\vcpkg integrate install
+echo "vcpkg integrated"
+
+.\vcpkg install opencv libxmlpp sdl2 sdl2-image sdl2-ttf boost-core boost-asio boost-system
+echo "Deps installed"
 
 cd %APPVEYOR_BUILD_FOLDER%
